03e495d35957de46967b96b3424935909996a6dc
cajunwildcat <elicheramie89@gmail.com>
2024-03-11 18:00:45 -0400

    adjusted parameters of columns scene

--- New Media Capstone Unity/Assets/Scripts/ColumnGen.cs
+++ New Media Capstone Unity/Assets/Scripts/ColumnGen.cs
@@ -7,9 +7,9 @@ public class ColumnGen : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        Vector3 start = new Vector3(-12, 6, 2);
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 20; j++) {
+        Vector3 start = new Vector3(-15.5f, 8.5f, 2);
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 18; j++) {
                 GameObject newCube = Instantiate(cube, start + new Vector3(i, -j, 4), Quaternion.identity);
                 newCube.transform.SetParent(transform, true);  
             }
