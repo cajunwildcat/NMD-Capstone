3a5953b961e0dac2a962920152147f86218e3e41
cajunwildcat <elicheramie89@gmail.com>
2024-04-02 09:39:08 -0400

    updated RiveTexture.cs and added RiveTest scene

--- New Media Capstone Unity/Assets/RiveStuff/RiveTexture.cs
+++ New Media Capstone Unity/Assets/RiveStuff/RiveTexture.cs
@@ -1,85 +1,60 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+
 using Rive;
 
-using LoadAction = UnityEngine.Rendering.RenderBufferLoadAction;
-using StoreAction = UnityEngine.Rendering.RenderBufferStoreAction;
-
-public class RiveTexture : MonoBehaviour
-{
-    public Rive.Asset asset;
-    public RenderTexture renderTexture;
-    public Material materialTest;
-
+public class RiveTexture : MonoBehaviour {
+    public Asset asset;
     public Fit fit = Fit.contain;
-    public Alignment alignment = Alignment.Center;
+    public int size = 512;
 
+    private RenderTexture _renderTexture;
     private Rive.RenderQueue m_renderQueue;
     private Rive.Renderer m_riveRenderer;
     private CommandBuffer m_commandBuffer;
 
-    private Rive.File m_file;
+    private File m_file;
     private Artboard m_artboard;
     private StateMachine m_stateMachine;
+    public StateMachine stateMachine => m_stateMachine;
 
-    private Camera m_camera;
-    private SpriteRenderer spriteRenderer;
+    private void Awake() {
+        _renderTexture = new RenderTexture(
+            size,
+             size,
+             0,
+             RenderTextureFormat.ARGB32
+         );
+        _renderTexture.enableRandomWrite = true;
 
-    private void Start()
-    {
-        // If on D3d11, this is required
+        UnityEngine.Renderer renderer = GetComponent<UnityEngine.Renderer>();
+        Material material = renderer.material;
+        material.mainTexture = _renderTexture;
 
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        if(spriteRenderer == null)
-        {
-            Debug.LogError("SpriteRenderer component not found.");
-            return;
-        }
-
-
-        materialTest.mainTexture = renderTexture;
-        gameObject.GetComponent<UnityEngine.Renderer>().material = materialTest;
-        renderTexture.enableRandomWrite = true;
-        m_renderQueue = new Rive.RenderQueue(renderTexture);
+        m_renderQueue = new Rive.RenderQueue(_renderTexture);
         m_riveRenderer = m_renderQueue.Renderer();
-        if (asset != null)
-        {
+        if (asset != null) {
             m_file = Rive.File.Load(asset);
             m_artboard = m_file.Artboard(0);
             m_stateMachine = m_artboard?.StateMachine();
         }
 
-        if (m_artboard != null && renderTexture != null)
-        {
-            m_riveRenderer.Align(fit, alignment, m_artboard);
-            materialTest.SetTexture("_BaseMap", renderTexture);
+        if (m_artboard != null && _renderTexture != null) {
+            m_riveRenderer.Align(fit, Alignment.Center, m_artboard);
             m_riveRenderer.Draw(m_artboard);
 
-           m_commandBuffer = new CommandBuffer();
-            m_commandBuffer.SetRenderTarget(renderTexture);
-            m_commandBuffer.ClearRenderTarget(true, true, UnityEngine.Color.clear);
+            m_commandBuffer = new CommandBuffer();
+            // m_renderQueue.toCommandBuffer();
+            m_commandBuffer.SetRenderTarget(_renderTexture);
+            m_commandBuffer.ClearRenderTarget(true, true, UnityEngine.Color.clear, 0.0f);
             m_riveRenderer.AddToCommandBuffer(m_commandBuffer);
-            m_camera = Camera.main;
-            if (m_camera != null)
-            {
-                Camera.main.AddCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
-            }
         }
     }
 
-    private void Update()
-    { 
-        if (m_stateMachine != null)
-        {
+    private void Update() {
+        m_riveRenderer.Submit();
+        if (m_stateMachine != null) {
             m_stateMachine.Advance(Time.deltaTime);
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (m_camera != null && m_commandBuffer != null)
-        {
-            m_camera.RemoveCommandBuffer(CameraEvent.AfterEverything, m_commandBuffer);
         }
     }
 }
