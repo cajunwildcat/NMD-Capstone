98031a335bd8d8344332e2e084576b866db9439c
cajunwildcat <elicheramie89@gmail.com>
2024-04-12 20:34:45 -0400

    fixed scene fade

--- New Media Capstone Unity/Assets/Scripts/SceneController.cs
+++ New Media Capstone Unity/Assets/Scripts/SceneController.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour {
     private static SceneController instance;
     public static SceneController Instance;
+
+    public Image fadePanel;
 
     private void Awake() {
         if (instance) {
@@ -22,10 +22,28 @@ public class SceneController : MonoBehaviour {
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Alpha1 ) && SceneManager.GetActiveScene().name != "Circles2") {
-            SceneManager.LoadScene("Circles2");
+            StartCoroutine(FadeToColor(0.5f, Color.black));
+            StartCoroutine(WaitFor(0.5f, () => { SceneManager.LoadScene("Circles2"); StartCoroutine(FadeToColor(0.5f, new Color(0, 0, 0, 0))); }));
         }
         if (Input.GetKeyDown(KeyCode.Alpha2) && SceneManager.GetActiveScene().name != "Columns") {
-            SceneManager.LoadScene("Columns");
+            StartCoroutine(FadeToColor(0.5f, Color.black));
+            StartCoroutine(WaitFor(0.5f, () => { SceneManager.LoadScene("Columns"); StartCoroutine(FadeToColor(0.5f, new Color(0, 0, 0, 0))); }));
+            
         }
+    }
+
+     IEnumerator FadeToColor(float time, Color newColor) {
+        float counter = 0;
+        Color startColor = fadePanel.color;
+        while (counter < time) {
+            counter += Time.deltaTime;
+            fadePanel.color = Color.Lerp(startColor, newColor, counter/time);
+            yield return null;
+        }
+    }
+
+    IEnumerator WaitFor(float time, Action postAction) {
+        yield return new WaitForSeconds(time);
+        postAction();
     }
 }
